a69e53b9407070989843d9b185833d9a7d7d0df8
Wal <gosoft@foxmail.com>
2022-12-02 22:14:59 +0800

    增加助手函数

--- app/common.php
+++ app/common.php
@@ -1,2 +1,13 @@
 <?php
-// 应用公共文件
+
+if (!function_exists('asset')) {
+    /**
+     * @param string $path
+     * @return string
+     */
+    function asset(string $path = ''): string
+    {
+        $root = request()->root(true);
+        return $root . '/' . ltrim($path, '/') . '?v=' . RELEASE;
+    }
+}
