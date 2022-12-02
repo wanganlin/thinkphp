6b06daab3ce58da8ad2978b803e25c19d3c7d251
Wal <gosoft@foxmail.com>
2022-12-02 22:52:53 +0800

    更新路由配置

--- route/app.php
+++ route/app.php
@@ -5,7 +5,7 @@ use think\facade\Route;
 $dirs = glob(app_path('controller/*'), GLOB_ONLYDIR);
 foreach ($dirs as $dir) {
     $m = basename($dir);
-    $v = $m === 'web' ? '' : $m;
+    $v = $m === config('app.default_app') ? '' : $m;
     Route::group($v, function () {
         Route::get(':c/:a', ':c/:a');
         Route::post(':c/:a', ':c/:a');
