05a9ead9ef614c6995e362070d8e4b2d07cdbd8a
Wal <gosoft@foxmail.com>
2022-12-07 15:04:59 +0800

    增加数据迁移

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call([
+            AuthRuleSeeder::class,
+            AuthGroupSeeder::class,
+            AuthGroupAccessSeeder::class,
+            UserSeeder::class,
+            SettingSeeder::class,
+        ]);
     }
 }
